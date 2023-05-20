51c9ba3152cf85eee0d053a3910035571f56ce42
dianakhudolii <di.khudolii@gmail.com>
2023-05-20 14:08:05 -0300

    Created start with checks(blacklisted, roles, debt), request for user contact (if hidden),possibility to upload the bill, and help function.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from telebot import types
 from auth_file import token  # import token
 import telebot
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def get_data_from_spreadsheet():
+def get_spreadsheet():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
     client = gspread.authorize(credentials)
 
     spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1ZnkTI5xrR0vDh5QWMyNEz4PWofWhMv-nc4oUyeEbkzQ/edit?usp=sharing'
     spreadsheet = client.open_by_url(spreadsheet_url)
+    return spreadsheet
+
+
+def get_data_from_spreadsheet():
+    spreadsheet = get_spreadsheet()
 
     blacklisted_sheet = spreadsheet.worksheet('blacklisted_numbers')
     blacklisted_data = blacklisted_sheet.get_all_records()
@@ -24,6 +30,13 @@ def get_data_from_spreadsheet():
     return blacklisted_data, tenants_data, admin_guard_data
 
 
+def get_debt_data_from_spreadsheet():
+    spreadsheet = get_spreadsheet()
+
+    debt_data = spreadsheet.worksheet('debt')
+    return debt_data
+
+
 def get_user_role(phone_number):
     blacklisted_data, tenants_data, admin_guard_data = get_data_from_spreadsheet()
 
@@ -33,7 +46,7 @@ def get_user_role(phone_number):
 
     for row in tenants_data:
         if phone_number in row[1:6]:
-            return "Tenant"
+            return "tenant"
 
     for i, row in enumerate(admin_guard_data):
         if row[0] == phone_number:
@@ -41,9 +54,29 @@ def get_user_role(phone_number):
 
     return None
 
+def get_apart_num(phone_number):
+    blacklisted_data, tenants_data, admin_guard_data = get_data_from_spreadsheet()
+
+    for i, row in enumerate(tenants_data):
+        if phone_number in row[1:6]:
+            return tenants_data[i][0]
+
+
+def check_debt(apartment_num):
+    debt_data = get_debt_data_from_spreadsheet()
+
+    for i, row in enumerate(debt_data):
+        if apartment_num == row[0]:
+            return int(debt_data[i][14])
+
 
 def initial_user_interface(role):
-    pass
+    if role == 'admin':
+        pass
+    if role == 'guard':
+        pass
+    if role == 'tenant':
+        pass
 
 
 def telegram_bot(token_value):
@@ -59,29 +92,62 @@ def telegram_bot(token_value):
         phone_number = contact.phone_number
 
         if phone_number is None:
-            #  Ask for phone number
-            pass
+
+            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+            button = types.KeyboardButton(text="Відправити свій контакт", request_contact=True)
+            keyboard.add(button)
+
+            bot.send_message(message.chat.id,
+                             "Натисніть кнопку 'Відправити контакт' для передачі свого особистого контакту.",
+                             reply_markup=keyboard)
+
+        else:
+            role = get_user_role(phone_number)
+
+            if role == 'tenant':
+                apartment_number = get_apart_num(phone_number)
+                debt = check_debt(apartment_number)
+
+                if debt > 240:
+                    bot.send_message(message.chat.id, f'У вас заборгованість {debt}, зверніться до адміністратора або завантажте квитанцію про оплату.')
+
+            answer = initial_user_interface(role)
+            bot.send_message(message.chat.id, answer)
+
+    @bot.message_handler(content_types=['contact'])
+    def handle_contact(message):
+        contact = message.contact
+        phone_number = contact.phone_number
 
         role = get_user_role(phone_number)
         answer = initial_user_interface(role)
+        bot.send_message(message.chat.id, answer)
+
+    @bot.message_handler(content_types=['photo'])
+    def handle_photo(message):
+        bot.reply_to(message, "Дякуємо! Ваша квитанція на розгляді в адміністратора.")
+
 
     @bot.message_handler(commands=['help'])
     def help_(message):
         help_message = '''
-        ВСТАВИТЬ ТЕКСТ СЮДА
+        Ось декілька корисних команд, які ви можете використовувати:
+        /start - Початок роботи з ботом
+        /help - Вивести це повідомлення з інструкціями
+        /newrequest - Створити нову заявку
+        /stop - Припинити використання бота
+
+        Якщо у вас виникли додаткові питання або проблеми, будь ласка, зверніться до адміністратора.
+
+        Дякуємо за використання цього бота!
         '''
+
         bot.send_message(message.chat.id, help_message)
 
-    bot.infinity_polling()  # start endless loop of receiving new messages from Telegram
+        bot.infinity_polling()  # start endless loop of receiving new messages from Telegram
 
 
 if __name__ == '__main__':
-    # telegram_bot(token)
-    # print(get_data_from_spreadsheet())
+    telegram_bot(token)
 
-    phone_number = '87654321'
-    user_role = get_user_role(phone_number)
-    if user_role is not None:
-        print(f"User role for phone number {phone_number}: {user_role}")
-    else:
-        print(f"User role not found for phone number {phone_number}")
+
